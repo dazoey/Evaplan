e0200e13a8fe42165ab2cd14c8d51f8476383c0c
dazoey <151373427+dazoey@users.noreply.github.com>
2025-11-26 21:42:12 +0700

    minor-u

--- frontend/src/api/eventApi.js
+++ frontend/src/api/eventApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api/events'
+  baseURL: 'http://localhost:4000/api/events'
 })
 
 export const getPublicEvents = () => API.get('/public')
