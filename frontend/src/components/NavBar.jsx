f9df71e2e48d84e0742ee805fd3b858f7aac571e
dazoey <151373427+dazoey@users.noreply.github.com>
2025-11-29 20:22:33 +0700

    navbar u

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,28 @@
 import { Link } from 'react-router-dom';
+import { FaHome, FaPlus, FaInfoCircle, FaUser } from 'react-icons/fa';
 
 export default function NavBar() {
+  const linkClass = "flex flex-col items-center text-sm hover:text-green-400 transition-colors p-2 md:p-0 md:flex-row md:hover:text-white md:hover:underline";
+  const iconClass = "w-5 h-5 mb-1 md:hidden";
+
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-gray-800 p-2 flex justify-around items-center md:static md:p-0 md:bg-transparent md:flex-row">
-      <Link to="/" className="text-sm hover:underline p-2 md:p-0">Home</Link>
-      <Link to="/add" className="text-sm hover:underline p-2 md:p-0">
-        + Add Event
+    <nav className="fixed bottom-0 left-0 right-0 bg-gray-800 p-2 flex justify-around items-center md:static md:p-0 md:bg-transparent md:flex-row md:space-x-4">
+      <Link to="/" className={linkClass}>
+        <FaHome className={iconClass} />
+        <span>Home</span>
       </Link>
-      <Link to="/about" className="text-sm hover:underline p-2 md:p-0">
-        About
+      <Link to="/add" className={linkClass}>
+        <FaPlus className={iconClass} />
+        <span className="md:hidden">Add</span>
+        <span className="hidden md:inline">+ Add Event</span>
       </Link>
-      <Link to="/profile" className="text-sm hover:underline p-2 md:p-0">
-        Profile
+      <Link to="/about" className={linkClass}>
+        <FaInfoCircle className={iconClass} />
+        <span>About</span>
+      </Link>
+      <Link to="/profile" className={linkClass}>
+        <FaUser className={iconClass} />
+        <span>Profile</span>
       </Link>
     </nav>
   );
