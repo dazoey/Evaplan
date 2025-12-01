83f3cb1e4a480c0b029fc6fc0c27715dc644fd60
dazoey <151373427+dazoey@users.noreply.github.com>
2025-12-01 20:51:25 +0700

    u

--- api/utils/validateEvent.js
+++
@@ -1,6 +0,0 @@
-export function validateEvent(body) {
-  if (!body.title || !body.start_time || !body.end_time) {
-    return { error: 'title, start_time, end_time wajib diisi' }
-  }
-  return { error: null }
-}
