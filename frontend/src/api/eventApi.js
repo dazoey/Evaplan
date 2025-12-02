80c8c5e1679db6bcd14e9e83fb636359cc91119c
dazoey <151373427+dazoey@users.noreply.github.com>
2025-12-02 08:39:41 +0700

    komen

--- frontend/src/api/eventApi.js
+++ frontend/src/api/eventApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 })
-
+// fungsi-fungsi buat manggil API terkait event (endpoint API)
 export const getPublicEvents = () => API.get('/api/events?public=true')
 export const getAllEvents = () => API.get('/api/events')
 export const getEventById = (id) => API.get(`/api/events/${id}`)
