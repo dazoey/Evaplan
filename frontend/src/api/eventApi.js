b4bf2494a53b4a837b1154bf99efbb63218efe6a
dazoey <151373427+dazoey@users.noreply.github.com>
2025-12-09 16:11:04 +0700

    endpoint fix

--- frontend/src/api/eventApi.js
+++ frontend/src/api/eventApi.js
@@ -4,7 +4,10 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 })
 // fungsi-fungsi buat manggil API terkait event (endpoint API)
-export const getPublicEvents = () => API.get('/api/events?public=true')
+export const getPublicEvents = (sortBy, order) => {
+  const params = { public: true, sortBy, order }
+  return API.get('/api/events', { params })
+}
 export const getAllEvents = () => API.get('/api/events')
 export const getEventById = (id) => API.get(`/api/events/${id}`)
 export const searchEvents = (params) => API.get('/api/events/search', { params })
