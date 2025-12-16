89b6bd0adfe47dc8b7793fdaebad49300a48acfb
dazoey <151373427+dazoey@users.noreply.github.com>
2025-12-17 00:43:32 +0700

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 export default function Profile() {
   return (
     <div>
-      <h2 className="text-green-500 text-lg mb-4">$ profile</h2>
+      <h2 className="text-green-500 text-lg mb-4">$profile</h2>
     </div>
   )
 }
