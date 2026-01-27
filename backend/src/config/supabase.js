48170cdc15f407b3893a2ed186f7f149b7f7e64e
dazoey <151373427+dazoey@users.noreply.github.com>
2026-01-27 18:38:47 +0700

    u

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv'
 // untuk akses supabase dari backend
 dotenv.config()
-
+//inisialisasi client supabase
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
